d3755f276837b5021d17b442e0f7f2049a621cfe
Tim van Cann <timvancann@gmail.com>
2025-01-11 07:04:51 +0100

    add migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20241222_075827_initial from './20241222_075827_initial';
+import * as migration_20250111_060402_initial from './20250111_060402_initial';
 
 export const migrations = [
   {
-    up: migration_20241222_075827_initial.up,
-    down: migration_20241222_075827_initial.down,
-    name: '20241222_075827_initial'
+    up: migration_20250111_060402_initial.up,
+    down: migration_20250111_060402_initial.down,
+    name: '20250111_060402_initial'
   },
 ];
